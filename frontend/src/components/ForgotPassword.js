043f317a2e846b71e1cb6da24d8bae6dec95f306
Prerna Prasad <prernaprasad100@gmail.com>
2022-04-15 00:43:28 +0530

    updated error display- ForgotPassword component

--- frontend/src/components/ForgotPassword.js
+++ frontend/src/components/ForgotPassword.js
@@ -1,22 +1,15 @@
-import React,{useState} from 'react'
-import logo from "../img/logo.png"
-import { Link } from 'react-router-dom'
+import React,{useState, useEffect} from 'react'
 import axios from '../axiosConfig';
 
-
 const ForgotPassword = () =>{
-  
-    const initialValues=[{
-        email:''
-    }]
 
-    const [email , setEmail] = useState(initialValues)
-    
-    function onChangeUserEmail (e){
-        let name =  e.target.name
-        let value =  e.target.value
-        console.log(`1 ${name}, ${value}`)
-        setEmail({...email, [name]:value})
+    const [email, setEmail] =useState('')
+    const [error , setError] = useState('')
+    const [success , setSuccess] = useState('')
+  
+    const handleChange=(e)=>{
+        console.log("handle change")
+        setEmail(e.target.value)
     }
     const handleSubmit = async(e)=>{
         e.preventDefault();
@@ -24,32 +17,39 @@ const ForgotPassword = () =>{
         console.log(`2 email `)
         console.log(`3 email:: ${email}`)
         console.log(`4 email:: JSON.stringify(email) `+JSON.stringify(email))
-        await axios.post('api/user/forgot-password/', email)
-            .then((res) => {
-                console.log(res.json(email))
-            }).catch((error) => {
-                console.log(error)
-            });
+        try{
+            const {data} = await axios.post('api/user/forgot-password/', {email})
+            console.log("5 data.message"+data.message)
+            setError('')
+            setSuccess(data.message)
+        }catch(error){
+            setSuccess('')
+            setError(error.response.data.message)
+            console.log("6 error.response.data.message",error.response.data.message)
+        };
         
-        console.log(`5 email :: JSON.stringify(email) `+JSON.stringify(email))
+        console.log("7")
     }
+    useEffect(()=>{
+    },[])
     return (
-        
-        <div className='text-bold box-border flex justify-center bg-gray-200 h-[450px] p-8'>
-            <div className=' mt-24 text-bold'>
-
-                 <form id ="forgot-pass"  className='px-36 py-8 bg-white rounded-md' onSubmit={handleSubmit}>
-                    
-                    <div>
-                        <label>Email</label>
-                        <input type="email" name="email" placeholder='Enter your email here' defaultValue='' onChange={onChangeUserEmail} className="px-3 my-2 border-2 rounded-md  border-violet-200" /> 
-                    </div>
-                    <div>
-                        <input type="submit" value="Send Password Reset link" className="bg-[#8751c4] hover:bg-violet-400 text-white font-bold py-2 px-4 mr-2 rounded-md" />
-                    </div>
-                </form>
+        <>
+            <div className='text-bold box-border flex justify-center bg-gray-200 h-[450px] p-8'>
+                <div className=' mt-24 text-bold'>
+                    <form id ="forgot-pass"  className='px-36 py-8 bg-white rounded-md' onSubmit={handleSubmit}>
+                        <div>
+                            <label>Email</label>
+                            <input type="email" name="email" placeholder='Enter your email here'  onChange={handleChange} className="px-3 my-2 border-2 rounded-md  border-violet-200" /> 
+                            {success && <p className='mt-0 text-green-600 opacity-100 text-sm pl-2 pb-4'>{success}</p>}
+                            {error && <p className='mt-0 text-pink-600 opacity-100 text-sm pl-2 pb-4'>{error}</p>}
+                        </div>
+                        <div>
+                            <input type="submit" value="Send Password Reset link" className="bg-[#8751c4] hover:bg-violet-400 text-white font-bold py-2 px-4 mr-2 rounded-md" />
+                        </div>
+                    </form>
                 </div>
             </div>
+        </> 
     )
 }
 
